ecc1a18a84df076147433e0197a565728c6b7fb3
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-02-17 11:49:37 -0500

    add REPO env var/logic

--- scripts/run-mrepo.sh
+++ scripts/run-mrepo.sh
@@ -2,11 +2,15 @@
 
 : ${UPDATE:=True}
 : ${WEB}
-
+: ${REPO}
 
 # Step1 Update the Repos - Defaults to 'True'
 if [ ${UPDATE} == 'True' ]; then
+  if [ -n ${REPO} ]; then
+    /usr/bin/mrepo -gu -r ${REPO}
+  else
     /usr/bin/mrepo -gu
+  fi
 fi
 
 # Create frozen repos
